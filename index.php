dcd89145fa56c41254dac536167dde05e04f63b7
antlogist <antlogist@gmail.com>
2021-12-02 20:05:56 +0300

    Header created

---
+++ index.php
@@ -0,0 +1,9 @@
+<?php
+
+/**
+ * Plugin Name: Media File Unzipper And Post Creator
+ * Description: WordPress plugin allows unzipping media files and creating posts.
+ * Version: 1.0
+ * Author: Anton Podlesnyy
+ * Author URI: https://podlesnyy.ru
+ */
