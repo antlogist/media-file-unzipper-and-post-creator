4bdeab83ccde5b57614ca92c32df01ccb16547b0
antlogist <antlogist@gmail.com>
2021-12-02 21:24:16 +0300

    Upload form created

--- index.php
+++ index.php
@@ -11,5 +11,22 @@
 add_action('admin_menu', 'add_menu_item');
 
 function add_menu_item() {
-  add_menu_page('Upload Media Zip', 'Upload Media Zip','manage_options','history_upload_media_zips','history_upload_media_zips','dashicons-media-archive', 10);
+  add_menu_page('Upload Wills', 'Upload Wills','manage_options','history_upload_wills','history_upload_wills','dashicons-media-archive', 10);
 }
+
+function history_upload_wills() { ?>
+  <div class="wrap">
+    <h1>Upload ZIP Archive with PDF files inside</h1>
+    <form action="/wp-admin/admin.php?page=history_upload_wills" method="post" enctype="multipart/form-data" class="server-form">
+
+      <p>
+        <input type="file" name="fileToUpload" id="fileToUpload">
+      </p>
+
+      <p class="submit">
+        <input type="submit" class="button button-primary" value="Upload and Create Posts" name="submit">
+			</p>
+
+    </form>
+  </div>
+<?php }
