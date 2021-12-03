54470cf457f9d48dfee2ba3fa95a077c0c26446c
antlogist <antlogist@gmail.com>
2021-12-03 19:41:11 +0300

    File path added

--- Classes/File.php
+++ Classes/File.php
@@ -1,11 +1,28 @@
 <?php
 
 class File {
+  private static $dir ;
+  private static $file;
+  private $fileName;
   private $isAllowed = false;
   private $type;
 
+  private static function init() {
+    //Get upload directory with year and month
+    self::$dir = "../../wp-content/uploads" . wp_upload_dir()['subdir'];
+
+    //Get file path
+    self::$file = self::$dir . '/' . basename($_FILES["fileToUpload"]["name"]);
+
+  }
+
   static function unzipAndPost() {
-    var_dump("Test");
+    self::init();
+    echo self::$file;
     exit;
+  }
+
+  private function uploadZip() {
+    // echo "../../wp-content/uploads" . wp_upload_dir()['subdir'];
   }
 }
