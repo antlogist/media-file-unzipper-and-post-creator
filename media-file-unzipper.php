652893501ec743a709290d188347a681fcd4f0cf
antlogist <antlogist@gmail.com>
2021-12-03 15:36:30 +0300

    Class UnzipFile created

---
+++ media-file-unzipper.php
@@ -0,0 +1,57 @@
+<?php
+
+/**
+ * Plugin Name: Media File Unzipper And Post Creator
+ * Description: WordPress plugin allows unzipping media files and creating posts.
+ * Version: 2.0.0
+ * Author: Anton Podlesnyy
+ * Author URI: https://podlesnyy.ru
+ */
+
+if ( ! defined( 'WPINC' ) ) {
+	die;
+}
+
+if (!defined('MEDIA_FILE_UNZIPPER_DIRNAME')) {
+  define('MEDIA_FILE_UNZIPPER_DIRNAME', plugin_basename(dirname(__FILE__)));
+}
+
+if (!defined('MEDIA_FILE_UNZIPPER')) {
+  define('MEDIA_FILE_UNZIPPER', '2.0.0');
+}
+
+if (!class_exists('UnzipFile')) {
+
+  require_once plugin_dir_path( __FILE__ ) . 'Classes/Render.php';
+  require_once plugin_dir_path( __FILE__ ) . 'Classes/File.php';
+
+  class UnzipFile {
+
+    private $slug = 'add_pdf_zip';
+
+    function __construct() {
+      add_action('admin_menu', array(&$this, 'addMenuItem'));
+
+      if(isset($_FILES['fileToUpload'])) {
+        $this->unzipAndPost();
+      }
+    }
+
+    public function addMenuItem() {
+      add_menu_page('Upload PDF', 'Upload PDF','manage_options', $this->slug , array($this, 'renderForm'),'dashicons-media-archive', 10);
+    }
+
+    public function renderForm() {
+      Render::form($this->slug);
+    }
+
+    private function unzipAndPost() {
+      File::unzipAndPost();
+    }
+
+  }
+
+  new UnzipFile();
+
+}
+
