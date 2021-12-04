83e000ff9c6aa67100c9af9639444f6d2f7737a9
antlogist <antlogist@gmail.com>
2021-12-04 10:08:44 +0300

    Posting in the media lib added

--- Classes/File.php
+++ Classes/File.php
@@ -6,8 +6,9 @@ class File {
   private static $dir ;
   private static $file;
   private static $fileName;
-  private $isAllowed = false;
-  private $type;
+  private static $numFiles;
+  private static $zip;
+  private static $allowedFileTypes = ['application/pdf'];
 
   private static function init() {
     //Get upload directory with year and month
@@ -56,25 +57,97 @@ class File {
 
       $zip->close();
       Render::message($message);
+      exit;
     }
+
+    self::$numFiles = $zip->numFiles;
 
     //Extract zip if it can be open
     $zip->extractTo(self::$dir);
+
+    self::$zip = $zip;
 
     $message = [];
 
     array_push($message, (object)[
       'type' => 'success',
-      'text' => 'The zip file' . self::$fileName . '  was successfully unzipped to' . wp_upload_dir()['url']
+      'text' => 'The zip file' . self::$fileName . '  was successfully unzipped to ' . wp_upload_dir()['url']
     ]);
 
     array_push($message, (object)[
       'type' => 'info',
-      'text' => 'There are ' . $zip->numFiles. ' files in this zip file.'
+      'text' => 'There are ' . self::$numFiles. ' files in this zip file.'
     ]);
 
     Render::message($message);
 
+    self::addToLibAndPost();
+  }
+
+  private static function addToLibAndPost() {
+    for($i=0; $i < self::$numFiles; $i++) {
+
+      //Get the URL of the media file.
+      $fileName = wp_upload_dir()['url'] . '/' . self::$zip->getNameIndex($i);
+
+      $message = [];
+      $message[] = (object)[
+        'type' => 'info',
+        'text' => 'File url: ' . $fileName
+      ];
+      Render::message($message);
+
+      //Get the file type
+      $fileType 	= wp_check_filetype( basename( $fileName ), null );
+
+      $message = [];
+      $message[] = (object)[
+        'type' => 'info',
+        'text' => 'File type: ' . $fileType['type']
+      ];
+      Render::message($message);
+
+      //Check the type
+      if(in_array($fileType['type'], self::$allowedFileTypes)) {
+
+        $message = [];
+        $message[] = (object)[
+          'type' => 'success',
+          'text' => '<a href="' . $fileName . '" target="_blank"> '. $fileName . '</a> File type: ' . $fileType['type']
+        ];
+        Render::message($message);
+
+
+        //Attachment information
+        $attachment = array(
+          'guid'           => $fileName,
+          'post_mime_type' => $fileType['type'],
+          'post_title'     => preg_replace('/\.[^.]+$/', '', self::$zip->getNameIndex($i)),
+          'post_content'   => '',
+          'post_status'    => 'inherit'
+        );
+
+        //Insert the attachment.
+        $attachId = wp_insert_attachment( $attachment, self::$dir . '/' . self::$zip->getNameIndex($i) );
+
+        //Generate attachment metadata
+        $attachData = wp_generate_attachment_metadata($attachId, self::$dir . '/' . self::$zip->getNameIndex($i));
+
+        //Update metadata for an attachment.
+        wp_update_attachment_metadata( $attachId, $attachData );
+
+      } else {
+
+        $message = [];
+        $message[] = (object)[
+          'type' => 'error',
+          'text' => self::$zip->getNameIndex($i) . ' could not be uploaded. Its file type of  ' . $fileType['type'] . ' is not allowed'
+        ];
+        Render::message($message);
+
+      }
+
+    }
   }
 
 }
